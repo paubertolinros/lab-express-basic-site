d80df75f17b5a035a577330d3ded0ca6eb2ce333
Pau Bertolin <paulaberos@gmail.com>
2022-12-14 16:36:14 +0100

    working on related page

--- app.js
+++ app.js
@@ -5,31 +5,24 @@ const hbs = require('hbs');
 
 app.use(express.static('public'));
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views')/*app.set('views', path.join(__dirname, 'views'));*/
+app.set('views', __dirname + '/views');/*app.set('views', path.join(__dirname, 'views'));*/
 hbs.registerPartials(__dirname + '/views/partials');
 
   const data =   [{
-    name: 'Pinkscape 01',
-    image: '/img/img1.jpg',
-    description: 'blblblblbllblb!',
-    Rate: '🖤🖤🖤'
+    name: 'Cottonbro CG Studio',
+    image: '/img/cottonbro-cg-studio.jpg',
   },
   {
-    name: 'Pinkscape 03',
-    image: 'https://cuevadelobo.com/wp-content/uploads/2018/12/Fallout-Juego-de-Mesa-Personajes-y-Tablero-1024x683.jpg',
-    description: 'jdbkjsdbkjabdkjad',
-    Rate:  '🖤🖤🖤'
+    name: 'Rodion Kutsaiev',
+    image: '/img/rodion-kutsaiev.jpg',
   },
   {
-    name: 'Pinkscape 05',
-    image: 'https://i.gzn.jp/img/2020/05/09/diceforge-rebellion/00.jpg',
-    description: 'jdbkjsdbkjabdkjad',
-  },
+    name: 'UZUNOV ROSTISLAV',
+    image: '/img/uzunov-rostislav.jpg',
+    },
   {
-    name: 'Pinkscape 10',
-    image: 'https://www.the-forge.it/wp-content/uploads/2020/08/zombicide-dark-side-03-1024x576.jpg',
-    description: 'jdbkjsdbkjabdkjad',
-    maxPlayers: 6
+    name: 'STEVE JOHNSON',
+    image: '/img/steve-johnson.jpg',
   }
 ]
 
@@ -49,10 +42,15 @@ app.get('/photo-gallery', (req, res, next) => {
   res.status(200).render('photo-gallery')
 })
 
-app.get('/tests', (req, res, next) => {
-  res.status(200).render('tests', { data })
-} )
+app.get('/related', (req, res, next) => {
+  
+  res.status(200).render('related', { data })
+})
+/*HELPER UPPERCASE*/
+hbs.registerHelper('toUpperCase', function(str) {
+  return str.toUpperCase();
+});
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log("hola 3000")
 })
